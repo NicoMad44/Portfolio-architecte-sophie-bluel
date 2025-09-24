4382449b0f64f377cf43a59fe552c6cffaaf174c
NicoMad44 <nicolas.madier@gmail.com>
2025-09-24 11:59:07 +0200

    Gallery Dynamicaly populated with works from api

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -6,3 +6,23 @@ const responseWorks = await fetch("http://localhost:5678/api/works");
 const works = await responseWorks.json();
 console.log(works);
 
+const galleryElement = document.querySelector(".gallery");
+
+// for loop to create a figure for each works that will be poplulating the gallery on the page
+for(let i=0; i<works.length;i++){
+    const figureElement = document.createElement("figure");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = works[i].imageUrl;
+    imgElement.alt = works[i].title;
+
+    const figureCaptionElement = document.createElement("figcaption");
+    figureCaptionElement.innerText = works[i].title;
+
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(figureCaptionElement);
+
+    galleryElement.appendChild(figureElement);
+}
+
+
